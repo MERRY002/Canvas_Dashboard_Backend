c445bc33e00cc840d6cd0d07408c4f783b0690ad
Merry Hayong <kumohime06@gmail.com>
2025-08-06 13:14:01 +0530

    updated backend

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import cors from "cors";
-import dotenv from "dotenv";
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 app.use(cors());
 
