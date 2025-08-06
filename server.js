bffb7c9d95c98a1c0be97f897f30d90c8401d871
Merry Hayong <kumohime06@gmail.com>
2025-08-06 13:18:30 +0530

    fixed backend

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 
-const server = createServer(app);
+const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "*",
